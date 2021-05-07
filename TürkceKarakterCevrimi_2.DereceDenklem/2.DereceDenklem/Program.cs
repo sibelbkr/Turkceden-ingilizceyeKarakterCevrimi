78853488d3516bdb2424d09187cb46c4d820d990
sibelbkr <sibelbkr23@outlook.com>
2021-05-07 21:44:53 +0300

    NewSmallApps

---
+++ TürkceKarakterCevrimi_2.DereceDenklem/2.DereceDenklem/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _2.DereceDenklem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            double delta;
+            double x1, x2;
+            double cyedek;
+
+            Console.WriteLine("ax2+bx+c türündeki 2.dereceden denklemin a,b,c değerlerini giriniz");
+
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("a=" + (double)a);
+            Console.WriteLine("b=" + (double)b);
+            Console.WriteLine("c=" + (double)c);
+
+
+            delta = (Math.Pow((double)b, (double)2)) - (4 * a * c);
+
+            Console.WriteLine("delta=" + delta);
+
+
+            if (delta < 0)
+            {
+                Console.WriteLine("DENKLEMİN REEL KÖKÜ YOKTUR");
+            }
+            else if (delta == 0)
+            {
+                cyedek = Math.Abs(c);
+                x1 = Math.Sqrt(cyedek);
+                Console.WriteLine("DENKLEMİN KÖKLERİ X1,X2 SIRASIYLA=" + x1 + "," + x1);
+            }
+            else
+            {
+                x1 = ((+b) + (Math.Sqrt((double)delta))) / (2 * a);
+                x2 = ((-b) - (Math.Sqrt((double)delta))) / (2 * a);
+                Console.WriteLine("DENKLEMİN KÖKLERİ X1,X2 SIRASIYLA=" + x1 + "," + x2);
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TürkceKarakterCevrimi_2.DereceDenklem/TukceKarakterCevirimi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TukceKarakterCevirimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("TÜRKÇE KARAKTERLER İÇEREN BİR METİN GİRİNİZ:");
+            string newstring = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("GİRİLEN METİN=" + newstring);
+
+
+            newstring = newstring.Replace('ı', 'i');
+            newstring = newstring.Replace('ç', 'c');
+            newstring = newstring.Replace('ğ', 'g');
+            newstring = newstring.Replace('ö', 'o');
+            newstring = newstring.Replace('ş', 's');
+            newstring = newstring.Replace('ü', 'u');
+            newstring = newstring.Replace('Ç', 'Ç');
+            newstring = newstring.Replace('Ğ', 'G');
+            newstring = newstring.Replace('Ö', 'O');
+            newstring = newstring.Replace('Ş', 'S');
+            newstring = newstring.Replace('Ü', 'U');
+
+            string editstring = newstring;
+            Console.WriteLine("DÜZENLENEN METİN=" + editstring);
+
+            Console.ReadKey();
+
+        }
+    }
+}
